8e7694e3f609b885ae730cfac46c4faab0d7df66
Sunny <swarnendughosh05@gmail.com>
2024-10-29 01:41:24 +0530

    shadcn added

--- apps/web/src/app/(landing)/layout.tsx
+++ apps/web/src/app/(landing)/layout.tsx
@@ -2,7 +2,7 @@ export default function landingLayout({children}: {
     children: React.ReactNode;
 }): JSX.Element {
     return (
-        <div className=" h-screen bg-gradient-to-r from-10% from-purple-600 via-60% via-transparent to-90% to-black">
+        <div className="h-screen bg-gradient-to-r from-10% from-purple-600 via-60% via-transparent to-90% to-black">
             {children}
         </div>
     )
--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div> 
-       Landing
+    <div className='bg-red-700'> 
+       hello
     </div>
   )
 }
